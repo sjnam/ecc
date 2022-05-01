ce809dad38244140a2a99e43f1064fe16715b8d8
sjnam <jsunam@gmail.com>
2022-05-01 17:36:47 +0900

    pollard rho

--- dlp.go
+++ dlp.go
@@ -3,6 +3,8 @@ package ecc
 import (
 	"crypto/elliptic"
 	"math/big"
+	"math/rand"
+	"time"
 )
 
 // Shanks' Baby-Step Giant-Step algorithm for ECDLP
@@ -37,6 +39,57 @@ func Shanks(curve ECurve, hx, hy *big.Int) int64 {
 }
 
 // PollardRho algorithm for the ECDLP
-func PollardRho(curve ECurve, hx, hy *big.Int) *big.Int {
-	return nil
+type ab struct {
+	ai *big.Int
+	bi *big.Int
+}
+
+// PollardRho algorithm for the ECDLP
+func PollardRho(c ECurve, hx, hy *big.Int) int64 {
+	htab := make(map[string]ab)
+	z := new(big.Int).Div(c.P, big.NewInt(3))
+	zz := new(big.Int).Add(z, z)
+	f := func(x, y, a, b *big.Int) (*big.Int, *big.Int, *big.Int, *big.Int) {
+		if x.Cmp(z) <= 0 { // S1
+			rx, ry := c.Add(c.Gx, c.Gy, x, y)
+			a.Add(a, big.NewInt(1))
+			return rx, ry, a.Mod(a, c.N), b
+		} else if x.Cmp(zz) <= 0 { // S2
+			rx, ry := c.ScalarMult(x, y, big.NewInt(2).Bytes())
+			a.Add(a, a)
+			b.Add(b, b)
+			return rx, ry, a.Mod(a, c.N), b.Mod(b, c.N)
+		} else { // S3
+			rx, ry := c.Add(hx, hy, x, y)
+			b.Add(b, big.NewInt(1))
+			return rx, ry, a, b.Mod(b, c.N)
+		}
+	}
+
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	a2, b2 := new(big.Int).Rand(rnd, c.N), new(big.Int).Rand(rnd, c.N)
+	aPx, aPy := c.ScalarBaseMult(a2.Bytes())
+	bQx, bQy := c.ScalarMult(hx, hy, b2.Bytes())
+	rx, ry := c.Add(aPx, aPy, bQx, bQy)
+	htab[string(elliptic.Marshal(c, rx, ry))] =
+		ab{new(big.Int).Set(a2), new(big.Int).Set(b2)}
+
+	for {
+		rx, ry, a2, b2 = f(rx, ry, a2, b2)
+		k := string(elliptic.Marshal(c, rx, ry))
+		if m, ok := htab[k]; ok {
+			a := m.ai
+			b := m.bi
+			a.Sub(a, a2)
+			a.Mod(a, c.N)
+			b2.Sub(b2, b)
+			b2.Mod(b2, c.N)
+			b2.ModInverse(b2, c.N)
+			a.Mul(a, b2)
+			a.Mod(a, c.N)
+			return a.Int64()
+		} else {
+			htab[k] = ab{new(big.Int).Set(a2), new(big.Int).Set(b2)}
+		}
+	}
 }
