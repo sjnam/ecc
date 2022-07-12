2ec76251526e8e665dfe553465a01e9a868f6225
sjnam <jsunam@gmail.com>
2022-07-13 06:01:24 +0800

    switch

--- dlp.go
+++ dlp.go
@@ -9,20 +9,24 @@ import (
 // PollardRho algorithm for the ECDLP
 func (c *EllipticCurve) PollardRho(px, py, hx, hy *big.Int) *big.Int {
 	N := c.N
+	one := big.NewInt(1)
+	two := big.NewInt(2)
+	three := big.NewInt(3)
+
 	f := func(x, y, a, b *big.Int) (*big.Int, *big.Int, *big.Int, *big.Int) {
-		k := new(big.Int).Mod(x, big.NewInt(3)).Int64()
-		if k == 0 { // S1: P+R, a+1, b
+		switch new(big.Int).Mod(x, three).Int64() {
+		case 0:
 			x, y = c.Add(px, py, x, y)
-			a.Add(a, big.NewInt(1))
+			a.Add(a, one)
 			return x, y, a.Mod(a, N), b
-		} else if k == 1 { // S2: 2R, 2a, 2b
-			x, y = c.ScalarMult(x, y, big.NewInt(2).Bytes())
+		case 1:
+			x, y = c.ScalarMult(x, y, two.Bytes())
 			a.Add(a, a)
 			b.Add(b, b)
 			return x, y, a.Mod(a, N), b.Mod(b, N)
-		} else { // S3: Q+R, a, b+1
+		default:
 			x, y = c.Add(hx, hy, x, y)
-			b.Add(b, big.NewInt(1))
+			b.Add(b, one)
 			return x, y, a, b.Mod(b, N)
 		}
 	}
@@ -66,6 +70,7 @@ func (c *EllipticCurve) PollardRho(px, py, hx, hy *big.Int) *big.Int {
 }
 
 func CRT(a, n []*big.Int) *big.Int {
+	one := big.NewInt(1)
 	p := new(big.Int).Set(n[0])
 	for _, n1 := range n[1:] {
 		p.Mul(p, n1)
@@ -74,7 +79,7 @@ func CRT(a, n []*big.Int) *big.Int {
 	for i, n1 := range n {
 		q.Div(p, n1)
 		z.GCD(nil, &s, n1, &q)
-		if z.Cmp(big.NewInt(1)) != 0 {
+		if z.Cmp(one) != 0 {
 			return nil
 		}
 		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
