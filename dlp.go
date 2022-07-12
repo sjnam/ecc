a7e7ee727bb86ed54dab74c125dc9580a72d025e
sjnam <jsunam@gmail.com>
2022-07-13 06:44:48 +0800

    add comments

--- dlp.go
+++ dlp.go
@@ -15,16 +15,16 @@ func (c *EllipticCurve) PollardRho(px, py, hx, hy *big.Int) *big.Int {
 
 	f := func(x, y, a, b *big.Int) (*big.Int, *big.Int, *big.Int, *big.Int) {
 		switch new(big.Int).Mod(x, three).Int64() {
-		case 0:
+		case 0: // S1: P+R, a+1, b
 			x, y = c.Add(px, py, x, y)
 			a.Add(a, one)
 			return x, y, a.Mod(a, N), b
-		case 1:
+		case 1: // S2: 2R, 2a, 2b
 			x, y = c.ScalarMult(x, y, two.Bytes())
 			a.Add(a, a)
 			b.Add(b, b)
 			return x, y, a.Mod(a, N), b.Mod(b, N)
-		default:
+		default: // S3: Q+R, a, b+1
 			x, y = c.Add(hx, hy, x, y)
 			b.Add(b, one)
 			return x, y, a, b.Mod(b, N)
@@ -70,7 +70,6 @@ func (c *EllipticCurve) PollardRho(px, py, hx, hy *big.Int) *big.Int {
 }
 
 func CRT(a, n []*big.Int) *big.Int {
-	one := big.NewInt(1)
 	p := new(big.Int).Set(n[0])
 	for _, n1 := range n[1:] {
 		p.Mul(p, n1)
@@ -79,7 +78,7 @@ func CRT(a, n []*big.Int) *big.Int {
 	for i, n1 := range n {
 		q.Div(p, n1)
 		z.GCD(nil, &s, n1, &q)
-		if z.Cmp(one) != 0 {
+		if z.Int64() != 1 {
 			return nil
 		}
 		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
