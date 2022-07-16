c19918632519b2275e2a45f657aa0e7f06f1a86d
sjnam <jsunam@gmail.com>
2022-07-16 22:55:21 +0800

    ec -> curve

--- crypto_test.go
+++ crypto_test.go
@@ -11,18 +11,18 @@ func TestSignAndVerify(t *testing.T) {
 	testAllCurves(t, testSignAndVerify)
 }
 
-func testSignAndVerify(t *testing.T, ec *Curve) {
-	priv, pubX, pubY, _ := elliptic.GenerateKey(ec, rand.Reader)
+func testSignAndVerify(t *testing.T, curve *Curve) {
+	priv, pubX, pubY, _ := elliptic.GenerateKey(curve, rand.Reader)
 
 	hashed := []byte("testing")
-	r, s := ec.Sign(priv, hashed)
+	r, s := curve.Sign(priv, hashed)
 
-	if !ec.Verify(pubX, pubY, hashed, r, s) {
+	if !curve.Verify(pubX, pubY, hashed, r, s) {
 		t.Errorf("Verify failed")
 	}
 
 	hashed[0] ^= 0xff
-	if ec.Verify(pubX, pubY, hashed, r, s) {
+	if curve.Verify(pubX, pubY, hashed, r, s) {
 		t.Errorf("Verify always works!")
 	}
 }
